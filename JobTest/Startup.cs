513c3e789f1c62bc78f29acc6cd2b06901e36595
MyOnAsSalat <89041155523@yandex.ru>
2019-06-27 21:13:46 +0800

    Add project files.

---
+++ JobTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JobTest.Startup))]
+namespace JobTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
